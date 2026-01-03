b727fc9735a91c1b964e8f8e7a7c0115942d7779
Kawadian <hanta_sho@icloud.com>
2026-01-03 14:03:08 +0900

    フォント周り改善

--- web/src/fonts/index.ts
+++ web/src/fonts/index.ts
@@ -1,11 +1,28 @@
-enum Font {
-  Digital7 = 'digital-7',
-  Poxel = 'poxel',
-  DINAlternateBold = 'din-alternate-bold',
-  Pretendard = 'pretendard',
-}
+// Dynamically import all font files from public/fonts
+const fontFiles = import.meta.glob('/public/fonts/*.{ttf,otf}', { eager: true, as: 'url' });
 
-// Load all fonts from the fonts public/fonts folder
-Object.values(Font).forEach((font) => new FontFace(font, `url(fonts/${font}.ttf)`).load().then((loadedFont) => document.fonts.add(loadedFont)));
+// Extract font names from file paths and create enum values
+const fontEntries = Object.keys(fontFiles).map((path) => {
+  const fileName = path.split('/').pop()!;
+  const fontName = fileName.replace(/\.(ttf|otf)$/, '');
+  // Convert kebab-case or regular names to PascalCase for enum keys
+  const enumKey = fontName
+    .split(/[-_]/)
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join('');
+  return [enumKey, fontName] as const;
+});
+
+// Create Font enum dynamically
+const Font = Object.fromEntries(fontEntries) as Record<string, string>;
+
+// Load all fonts
+Object.values(Font).forEach((fontName) => {
+  const fontFile = Object.entries(fontFiles).find(([path]) => path.includes(fontName));
+  if (fontFile) {
+    const [, url] = fontFile;
+    new FontFace(fontName, `url(${url})`).load().then((loadedFont) => document.fonts.add(loadedFont));
+  }
+});
 
 export default Font;
